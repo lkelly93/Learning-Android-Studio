325c47c5ef5d216be9ede72702c088f56166a512
Luke Kelly <lukejelly93@gmail.com>
2020-06-08 09:30:08 -0700

    Testing git

--- app/src/main/java/com/example/learningas/MainActivity.java
+++ app/src/main/java/com/example/learningas/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.learningas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.learningas.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
+
+//    /** Called when the user taps the Send button  */
+//    public void sendMessage(View view){
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//
+//    }
 }
